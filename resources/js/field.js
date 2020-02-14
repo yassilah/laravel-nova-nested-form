475da1ef54c9bec242f2eb4371d977065bb5b44f
girishxtec <girish@xtecsolutions.com>
2020-02-14 17:47:35 +0530

    bug fixed  #93

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,11 +1,11 @@
 import NestedFormField from './components/NestedFormField'
-//import NestedFormBelongsToField from './components/CustomFields/BelongsToField'
-//import NestedFormFileField from './components/CustomFields/FileField'
-//import NestedFormMorphToField from './components/CustomFields/MorphToField'
+import NestedFormBelongsToField from './components/CustomFields/BelongsToField'
+import NestedFormFileField from './components/CustomFields/FileField'
+import NestedFormMorphToField from './components/CustomFields/MorphToField'
 
 Nova.booting((Vue, router, store) => {
   Vue.component('form-nested-form', NestedFormField)
-//  Vue.component('form-nested-form-belongs-to-field', NestedFormBelongsToField)
-//  Vue.component('form-nested-form-morph-to-field', NestedFormMorphToField)
-//  Vue.component('form-nested-form-file-field', NestedFormFileField)
+  Vue.component('form-nested-form-belongs-to-field', NestedFormBelongsToField)
+  Vue.component('form-nested-form-morph-to-field', NestedFormMorphToField)
+  Vue.component('form-nested-form-file-field', NestedFormFileField)
 })
